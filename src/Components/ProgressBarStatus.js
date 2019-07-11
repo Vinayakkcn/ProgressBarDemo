7b64562b1f49d7e942dcb54a30e25eca4aae22ad
vinayakkcn <vinayakkchandran@gmail.com>
2019-07-12 00:03:02 +0530

    color changing feature added

--- src/Components/ProgressBarStatus.js
+++ src/Components/ProgressBarStatus.js
@@ -4,13 +4,19 @@ import { MDBProgress } from "mdbreact";
 import "../styles/Progressbar.scss";
 
 const ProgressBarStatus = props => {
+  // if((props.barValue / props.limit) * 100>75) {
+  //   props.color = "danger";
+  // }
+  // else{
+  //   props.color = "success";
+  // }
   return (
     <>
       <MDBProgress
         material
-        value={props.barValue}
-        style={{backgroundColor:(props.barValue / props.limit) * 100}>75? "red": "blue"}
-        className = "progressBar" 
+        value={((props.barValue / props.limit) * 100).toFixed(0)}
+        color={(props.barValue / props.limit) * 100 > 75 ? "danger" : {}}
+        className="progressBar"
       >
         {((props.barValue / props.limit) * 100).toFixed(0)}
       </MDBProgress>
